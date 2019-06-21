a55c256ce68b34b44fec8bc7769f40e1523766d7
jkapl <joelkaplan1@gmail.com>
2019-06-21 16:46:49 -0700

    pass cookie parser tests

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,23 @@
 const parseCookies = (req, res, next) => {
+  let cookies = req.headers;
+  var splitCookArr = [];
+  var cookieObj = {};
+
+  if (cookies.cookie) {
+    var cookieArray = cookies.cookie.split('; ');
+    for (var i=0; i<cookieArray.length; i++) {
+      splitCookArr.push(cookieArray[i].split('='));
+    }
+    for (var i=0;i<splitCookArr.length; i++) {
+      cookieObj[splitCookArr[i][0]] = splitCookArr[i][1];
+    }
+  }
+  req.cookies = cookieObj;
+  next();
+  res.send(req);
+  
+  
+  
 };
 
 module.exports = parseCookies;
