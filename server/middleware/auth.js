82aa40bfd658513e6e41c665c380243932805b48
jkapl <joelkaplan1@gmail.com>
2019-06-22 11:17:08 -0700

    pass all tests for session parser

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -21,8 +21,27 @@ module.exports.createSession = (req, res, next) => {
   } else {
     models.Sessions.get({hash:req.cookies['shortlyid']})
       .then((sess)=>{
-        req.session = { hash: sess.hash };
-        next();
+        req.session = { hash : sess.hash };
+        if (sess.userId !== null) {
+          models.Users.get({id:sess.userId})
+            .then((userData)=> {
+              console.log(userData);
+              req.session.userId = sess.userId;
+              req.session.user = {};
+              req.session.user.username = userData.username;
+              next();
+            });
+        } else {
+          next();
+        }
+      }).catch(()=> {
+        models.Sessions.create()
+          .then((sess)=> models.Sessions.get({id: sess.insertId}))
+          .then((data)=> {
+            res.cookie('shortlyid', data.hash);
+            req.session = { hash: data.hash };
+            next();
+          });
       });
   }
   // next();
--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -447,13 +447,14 @@ describe('', function() {
           createSession(requestWithoutCookie, response, function() {
             var hash = requestWithoutCookie.session.hash;
             db.query('UPDATE sessions SET userId = ? WHERE hash = ?', [userId, hash], function(error, result) {
-
+              
               var secondResponse = httpMocks.createResponse();
               var requestWithCookies = httpMocks.createRequest();
               requestWithCookies.cookies.shortlyid = hash;
-
+              
               createSession(requestWithCookies, secondResponse, function() {
                 var session = requestWithCookies.session;
+                
                 expect(session).to.be.an('object');
                 expect(session.user.username).to.eq(username);
                 expect(session.userId).to.eq(userId);
